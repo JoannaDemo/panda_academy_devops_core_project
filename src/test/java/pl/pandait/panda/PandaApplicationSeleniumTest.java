0eefd40723077095c01139d284bad83378b0f320
JoannaDemo <69588787+JoannaDemo@users.noreply.github.com>
2020-09-14 12:25:28 +0200

    Update PandaApplicationSeleniumTest.java

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -22,7 +22,7 @@ public class PandaApplicationSeleniumTest {
     private static WebDriver driver;
     
     @LocalServerPort
-    private int port;
+    private int port=8090
     
     @BeforeEach
     public void startup() throws InterruptedException, MalformedURLException {
